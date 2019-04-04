157793c360996aced2831a80fb9cc57ff664d9bf
GilZ <gil.zieder@gmail.com>
2019-04-04 16:46:50 +0300

    Added combined tweets, refactor

--- src/politibot/main.py
+++ src/politibot/main.py
@@ -1,5 +1,5 @@
 from twitter_handler import TwitterHandler
-from tweet_maker import TweetMaker
+import tweet_maker
 import config_manager
 from os import path, listdir
 import argparse
@@ -20,20 +20,28 @@ def run():
         tweeter_handler = TwitterHandler()
         tweeter_handler.fetch_all(original_tweets_dir)
 
+    combined_model = None
     with open(new_tweets_file, 'w', encoding='utf8') as output:
         for filename in listdir(original_tweets_dir):
             with open(path.join(original_tweets_dir, filename), encoding='utf8') as f:
                 original_tweets = f.read()
-                new_tweets = TweetMaker.make_tweets(original_tweets, 10)
-                for new_tweet in new_tweets:
-                    if not new_tweet:
-                        continue
+                new_tweets, model = tweet_maker.make_tweets_from_text(original_tweets, 10)
+                combined_model = tweet_maker.combine(combined_model, model)
+                output_tweets(new_tweets, filename, output, args.post)
+        new_combined_tweets = tweet_maker.make_tweets_from_model(combined_model, 10)
+        output_tweets(new_combined_tweets, config['bot_screen_name'], output, args.post)
 
-                    final_new_tweet = new_tweet + " – " + filename
-                    if args.post:
-                        pass  # TODO
-                    output.write(final_new_tweet + '\n')
-                output.write('\n')
+
+def output_tweets(tweets, twitter_handle, output, should_post):
+    for new_tweet in tweets:
+        if not new_tweet:
+            continue
+
+        final_new_tweet = new_tweet + " – " + twitter_handle
+        if should_post:
+            pass  # TODO
+        output.write(final_new_tweet + '\n')
+    output.write('\n')
 
 
 if __name__ == '__main__':
--- src/politibot/tweet_maker.py
+++ src/politibot/tweet_maker.py
@@ -1,8 +1,16 @@
 import markovify
 
 
-class TweetMaker:
-    @staticmethod
-    def make_tweets(text, count):
-        text_model = markovify.Text(text)
-        return [text_model.make_short_sentence(240) for _ in range(count)]
+def make_tweets_from_model(model, count):
+    return [model.make_short_sentence(240) for _ in range(count)]
+
+
+def make_tweets_from_text(text, count):
+    model = markovify.Text(text)
+    return make_tweets_from_model(model, count), model
+
+
+def combine(combined_model, model):
+    if combined_model is None:
+        return model
+    return markovify.combine(models=[combined_model, model])
